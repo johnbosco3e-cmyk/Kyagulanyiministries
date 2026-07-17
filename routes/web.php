c20fe93f337e9b120e229ab9ce8db9b3f663a380
johnbosco3e-cmyk <john.bosco3e@gmail.com>
2026-07-17 16:42:03 +0300

    Add Laravel routes for all legacy pages

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-foreach (['about','programs','projects','news','volunteer','contact','donate'] as $page) {
+foreach (['about','events','news','media','volunteer','contact','prayer-request','donate','campaigns','partners','reports','transparency','privacy','terms'] as $page) {
     Route::view('/'.$page, 'page', ['page' => ucfirst($page)]);
 }
+Route::view('/programs', 'page', ['page' => 'Programs']);
+Route::view('/programs/{slug}', 'page', ['page' => 'Program']);
+Route::view('/projects', 'page', ['page' => 'Projects']);
+Route::view('/projects/{slug}', 'page', ['page' => 'Project']);
