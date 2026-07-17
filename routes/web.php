f12eb7db9b7ff787cded68329861f0568e08360f
johnbosco3e-cmyk <john.bosco3e@gmail.com>
2026-07-17 16:24:51 +0300

    Migrate core pages to Laravel Blade

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+foreach (['about','programs','projects','news','volunteer','contact','donate'] as $page) {
+    Route::view('/'.$page, 'page', ['page' => ucfirst($page)]);
+}
